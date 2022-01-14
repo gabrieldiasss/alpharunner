9190029af4a2f20603e186ae3c92e579c0200697
Gabriel  Dias <gabrieldiascorrea7@gmail.com>
2022-01-14 15:07:47 -0300

    load products

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,21 +1,51 @@
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
+
+import { api } from '../../services/api'
+import { Container } from './styles'
+
+interface Products {
+    id: number;
+    title: string;
+    price: number;
+    image: string; 
+}
 
 const Home = (): JSX.Element => {
+
+    const [products, setProducts] = useState<Products[]>([])
+    const [counter, setCounter] = useState(0)
 
     useEffect(() => {
 
         async function LoadProducts() {
-            
+            const response = await api.get('products')
+
+            setProducts(response.data)
         }
 
         LoadProducts()
 
     }, [])
 
+    /* function handleAddProduct(id: number) {
+        
+    } */
+
     return (
-        <div>
-            <h1>Aqui ficara os produtos</h1>
-        </div>
+        <Container>
+            {products.map(product => (
+                <li key={product.id}>
+                    <img src={product.image} alt="" />
+                    <h2>{product.title}</h2>
+                    <h3>{product.price}</h3>
+
+                    <button onClick={() => handleAddProduct(product.id)} >
+                        {counter} Adicionar ao carrinnho
+                    </button>
+                </li>
+            ))}
+
+        </Container>
     )
 }
 
